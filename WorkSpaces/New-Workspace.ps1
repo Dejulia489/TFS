7a34d4b2f23e6033d1ea9fb72689306b863057fe
Michael Dejulia <Dejulia489@users.noreply.github.com>
2017-09-08 11:48:32 -0700

    TFS WorkSpace Helper Functions

---
+++ WorkSpaces/New-Workspace.ps1
@@ -0,0 +1,74 @@
+﻿<#
+    .Synopsis
+    Remove TFS Workspace 
+#>
+
+Function New-Workspace
+{
+    [CmdletBinding()]
+    param
+    (
+        [String]
+        [Parameter(Mandatory = $true, 
+            ValueFromPipelineByPropertyName = $true)]
+        $WorkspaceName,
+
+        [String]
+        [Parameter(Mandatory = $false, 
+            ValueFromPipelineByPropertyName = $true)]
+        $ComputerName,
+
+        [String]
+        [Parameter(Mandatory = $false)]
+        $Template,
+        
+        [String]
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('Private', 'PublicLimited', 'Public')]
+        $Permission,
+
+        [String]
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('Local', 'Server')]
+        $Location,
+
+        [Parameter(Mandatory = $false, 
+            ValueFromPipelineByPropertyName = $true)]
+        [String]
+        $Collection,
+
+        [pscredential]
+        [Parameter(Mandatory = $false)]
+        $Credential
+    )
+
+    $Tfexe = 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\TF.exe'
+
+    $arguments = @(
+        'workspace'
+        '/New'
+        '/NoPrompt'
+        '/Collection:{0}' -f $Collection
+        '/Location:{0}' -f $Location
+        '{0}' -f $WorkspaceName 
+    )
+    If ($ComputerName)
+    {
+        $arguments += '/Computer:{0}' -f $ComputerName
+    }
+    If ($Template)
+    {
+        $arguments += '/Template:{0}' -f $Template
+    }
+    If ($Permission)
+    {
+        $arguments += '/Permission:{0}' -f $Permission
+    }
+    If ($Credential)
+    {
+        $arguments += '/Login:{0},{1}' -f $Credential.UserName, $Credential.GetNetworkCredential().Password
+    }
+
+    . $Tfexe $arguments
+}
+
---
+++ WorkSpaces/Remove-Workspace.ps1
@@ -0,0 +1,44 @@
+﻿<#
+    .Synopsis
+    Remove TFS Workspace 
+#>
+Function Remove-Workspace
+{
+    [CmdletBinding(SupportsShouldProcess = $true,
+        ConfirmImpact = 'High')]
+    param
+    (
+        [String]
+        [Parameter(Mandatory = $true, 
+            ValueFromPipelineByPropertyName = $true)]
+        $WorkspaceName,
+
+        [Parameter(Mandatory = $false, 
+            ValueFromPipelineByPropertyName = $true)]
+        [String]
+        $Collection,
+
+        [pscredential]
+        [Parameter(Mandatory = $false)]
+        $Credential
+    )
+
+    $Tfexe = 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\TF.exe'
+
+    $arguments = @(
+        'workspace'
+        '/Collection:{0}' -f $Collection
+        '{0}' -f $WorkspaceName 
+    )
+    If ($Credential)
+    {
+        $arguments += '/Login:{0},{1}' -f $Credential.UserName, $Credential.GetNetworkCredential().Password
+    }
+    If ($PSCmdlet.ShouldContinue("This will remove the workspace: [$WorkspaceName] from Collection: [$Collection], are you sure?", 'Remove'))
+    {
+        $arguments += '/delete'
+    }
+        
+    . $Tfexe $arguments
+}
+
